b26e68dfba5823a4ccaf9b592dbd7ff5c2d47c5b
LensKase23 <73784825+LensKase23@users.noreply.github.com>
2020-11-06 05:03:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const qrcode = require("qrcode-terminal");
 const moment = require("moment");
 const cheerio = require("cheerio");
 const get = require('got')
+const fs = require("fs");
 const dl = require("./lib/downloadImage.js");
 const fetch = require('node-fetch');
 const urlencode = require("urlencode");
